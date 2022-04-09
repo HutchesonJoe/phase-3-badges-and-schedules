de8922d4790da1ba487806891105406fdb25e2e0
Joe <hutcheson2001@yahoo.com>
2022-04-09 16:56:08 -0400

    commit tree e3ae72ec150623a980c38ff710516ae19217cd38

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  arr.map {|n| "Hello, my name is #{n}."}
+end
+
+def assign_rooms(arr)
+  room = 0
+  arr.map {|n| "Hello, #{n}! You'll be assigned to room #{room += 1}!"}
+end
+
+def printer(arr)
+  batch_badge_creator(arr).map {|item| puts item}
+  assign_rooms(arr).map{|item| puts item}
+end
